ac63caeaefaabaa8c044553ba09ee67ba1777fcf
Simon <69726466+SimonPe25@users.noreply.github.com>
2020-12-13 19:06:53 +0200

    ret

--- dist/js/script-min.js
+++ dist/js/script-min.js
@@ -1,10 +1,14 @@
-const btn = document.querySelector(".header-list__btn");
+const btn = document.querySelector(".header-list_btn");
 const menu = document.querySelector(".header-list");
-const item = document.getElementsByClassName("header-list__item");
+const item = document.getElementsByClassName("header-list_item");
+const btnActive = document.querySelector(".header-list_btn");
 
 btn.addEventListener("click",function(){
     menu.classList.toggle("active");
-    btn.classList.toggle("active");
+});
+
+btnActive.addEventListener("click",function(){
+    btnActive.classList.toggle("active");
 });
 for (let index = 0; index < item.length; index++) {
     item[index].addEventListener("click",function(){
